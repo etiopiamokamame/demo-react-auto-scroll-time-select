ca09257d0e294020a64b4026bbaa5780b6763c1e
etiopiamokamame <haimaki4222@gmail.com>
2020-04-24 23:46:38 +0900

    Updates

---
+++ precache-manifest.da7dc09f0880d067ce3d2e38b7a344e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b0b16e6428f75909ef2f19d440f27d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abec711440a8112b8291",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82001683c38705c8001a",
+    "url": "/static/js/2.9373454f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9373454f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abec711440a8112b8291",
+    "url": "/static/js/main.fe487d07.chunk.js"
+  },
+  {
+    "revision": "ed63b1e5a08683a792ac",
+    "url": "/static/js/runtime-main.b8562f62.js"
+  }
+]);
