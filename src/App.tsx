45e9b366e8a40aaafd9c503020759bc1b494701e
etiopiamokamame <haimaki4222@gmail.com>
2021-04-04 17:02:41 +0900

    npm version up

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,12 @@ import Select, { OptionType } from "react-auto-scroll-time-select";
 function App() {
   const [option, setOption] = useState<OptionType | null>();
 
-  const handleChange = (value: any) => {
-    setOption(value);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <div style={{ padding: "10px 0" }}>{option && <>{option.value}</>}</div>
         <div style={{ padding: "10px 0" }}>
-          <Select onChange={handleChange} value={option} />
+          <Select onChange={setOption} value={option} />
         </div>
       </header>
     </div>
